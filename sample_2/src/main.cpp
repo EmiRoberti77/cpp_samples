87dd7004231074116e544a27c376347e22ad5706
EmiRoberti77 <emiliano.roberti@accenture.com>
2024-05-19 12:34:44 +0100

    initialization code

--- sample_2/src/main.cpp
+++ sample_2/src/main.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
 
+using namespace std;
 void print(int val);
+
+class MyClass {
+  public:
+    int value;
+
+  MyClass(){
+    cout << "init MyClass 0 value" << endl;
+  };
+  MyClass(int val): value(val){
+    cout << "init MyClass " << val << endl;     
+  }
+
+  void doSomething(){
+    cout << value << endl;
+  }
+};
 
 int main(){
   //value initialization
   int valueInt = int();
 
-  //directtion initialization
+  //direction initialization
   int directInt(23);
 
-  //copy initializarion
+  //copy initialization
   int copyInt = 12;
 
   print(valueInt);
   print(directInt);
   print(copyInt);
+
+  MyClass myClassCopyInit = MyClass();
+  MyClass myClassDirectionInit(12);
+  MyClass mClassCopyInit = myClassDirectionInit; 
+  myClassCopyInit.doSomething();
+  myClassDirectionInit.doSomething();
+  
   return 0;
 }
 
 void print(int val){
-  using namespace std;
   cout << val << endl;
 }
